680e5a73f5a3ee1eb84825e2e8a34fd4decc5610
Abrar-Islam-Oitijjho <abraroitijjho35@gmail.com>
2021-07-13 22:12:03 +0600

    existing work uploaded

---
+++ Existing work/tools_copy/settings.py
@@ -0,0 +1,16 @@
+import os
+
+dataset_path = "E:\Project 103\Data"
+death_path = os.path.join(dataset_path, "death/")
+kill_path = os.path.join(dataset_path, "kill/")
+NoAction_path = os.path.join(dataset_path, "NoAction/")
+#t_path = os.path.join(dataset_path, "t/")
+train_frames_path_name = 'train_frames'
+val_frames_path_name = 'val_frames'
+test_frames_path_name = 'test_frames'
+train_frames_path = os.path.join(dataset_path, train_frames_path_name, "")
+val_frames_path = os.path.join(dataset_path, val_frames_path_name, "")
+test_frames_path = os.path.join(dataset_path, test_frames_path_name, "")
+
+val_dataset_ratio = 0.2
+test_dataset_ratio = 0.2
